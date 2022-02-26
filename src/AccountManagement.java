4da9514eed90eb2081b7f2b9845d79143b49defa
Darla David <201910063@feualabang.edu.ph>
2022-02-26 20:17:58 +0800

    Fixed GUI

--- src/AccountManagement.java
+++ src/AccountManagement.java
@@ -10,7 +10,7 @@ public class AccountManagement extends JFrame {
     JTextField tfstudentID, tffullName, tfaddress, tfemail, tfcontact; //textbox
     JComboBox<String> ddcourse; //dropdown
     JComboBox<Integer> ddyearLvl;
-    JButton btnClear; //btnUpdate, btnExit, btnNext, btnPrevious; //buttons
+    JButton btnClear, btnUpdate, btnExit, btnNext, btnPrevious; //buttons
     //ArrayList<Student> studentList;
     int currIndex;
 
@@ -25,7 +25,7 @@ public class AccountManagement extends JFrame {
         lbemail=new JLabel("Email:");
         lbcontact=new JLabel("Contact Number:");
 
-        /* Label Fonts and Bounds */
+        /* Label Fonts and Size */
         lbWelcome.setFont(mainFont);
         lbstudentID.setFont(mainFont);
         lbfullName.setFont(mainFont);
@@ -35,14 +35,14 @@ public class AccountManagement extends JFrame {
         lbemail.setFont(mainFont);
         lbcontact.setFont(mainFont);
 
-        lbWelcome.setBounds(1,1,200,20);
+        /*lbWelcome.setBounds(1,1,200,20);
         lbstudentID.setBounds(1,40,150,20);
         lbfullName.setBounds(1,80,150,20);
         lbcourse.setBounds(1,120,150,20);
         lbyearLvl.setBounds(1,160,150,20);
         lbaddress.setBounds(1,200,150,20);
         lbemail.setBounds(1,240,150,20);
-        lbcontact.setBounds(1,280,150,20);
+        lbcontact.setBounds(1,280,150,20);*/
 
         /* Text Fields */
         tfstudentID = new JTextField("");
@@ -58,81 +58,74 @@ public class AccountManagement extends JFrame {
         tfemail.setFont(mainFont);
         tfcontact.setFont(mainFont);
 
-        tfstudentID.setBounds(155,40,200,20);
-        tffullName.setBounds(155,80,200,20);
-        tfaddress.setBounds(155,200,200,20);
-        tfemail.setBounds(155,240,200,20);
-        tfcontact.setBounds(155,280,200,20);
+        tfstudentID.setPreferredSize(new Dimension(450,50));
+        /*tfstudentID.setBounds(155,120,150,20);
+        tffullName.setBounds(155,120,150,20);
+        tfaddress.setBounds(155,120,150,20);
+        tfcontact.setBounds(155,120,150,20);*/
 
         /* Dropdown */
         String courseArr[]={"BSCS-SE","BSCS-CPP","BSCS-DS","BSCS-NET"};
         ddcourse=new JComboBox<String>(courseArr);
-        ddcourse.setBounds(155,120,150,20);
+        //ddcourse.setBounds(155,120,150,20);
 
         Integer levelArr[]={1,2,3,4,5,6};
         ddyearLvl=new JComboBox<Integer>(levelArr);
-        ddyearLvl.setBounds(155,160,50,20);
+        //ddyearLvl.setBounds(155,160,50,20);
 
         /* Buttons */
-        btnClear = new JButton("Clear");
-        btnClear.setFont(mainFont);
-        btnClear.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                tfstudentID.setText("");
-                tffullName.setText("");
-                tfaddress.setText("");
-                tfcontact.setText("");
-                tfemail.setText("");
-            }
-        });
-
-        /* Layout */
-        JPanel formPanel = new JPanel();
-        formPanel.setLayout(new GridLayout(4, 1, 5, 5));
-        formPanel.add(lbWelcome);
-        formPanel.add(lbstudentID);
-        formPanel.add(lbfullName);
-        formPanel.add(lbcourse);
-        formPanel.add(lbyearLvl);
-        formPanel.add(lbaddress);
-        formPanel.add(lbemail);
-        formPanel.add(lbcontact);
+        btnPrevious=new JButton("<<");
+        btnPrevious.setBounds(20,320,150,20);
+        //btnPrevious.addActionListener(this);
         
+        btnNext=new JButton(">>");
+        btnNext.setBounds(230,320,150,20);
+        //btnNext.addActionListener(this);
+        
+        btnUpdate=new JButton("Update");
+        btnUpdate.setBounds(20,360,100,20);
+        //btnUpdate.addActionListener(this);
+        
+        btnExit=new JButton("Exit");
+        btnExit.setBounds(230,360,100,20);
+        //btnExit.addActionListener(this);
+
+        /* Panels*/
+        JPanel formPanel = new JPanel();
+        formPanel.setLayout(new GridLayout(7,2)); //Try gridbaglayout
+        formPanel.add(lbstudentID);
         formPanel.add(tfstudentID);
+        formPanel.add(lbfullName);
         formPanel.add(tffullName);
-        formPanel.add(tfaddress);
-        formPanel.add(tfemail);
-        formPanel.add(tfcontact);
+        formPanel.add(lbcourse);
         formPanel.add(ddcourse);
+        formPanel.add(lbyearLvl);
         formPanel.add(ddyearLvl);
-        formPanel.add(btnClear);
-        /*formPanel.add(btnNext);
-        formPanel.add(btnUpdate);
-        formPanel.add(btnExit);*/
+        formPanel.add(lbaddress);
+        formPanel.add(tfaddress);
+        formPanel.add(lbemail);
+        formPanel.add(tfemail);
+        formPanel.add(lbcontact);
+        formPanel.add(tfcontact);
 
         formPanel.setOpaque(false);
-        formPanel.add(lbstudentID);
-        formPanel.add(tfstudentID);
-        formPanel.add(lbfullName);
-        formPanel.add(tffullName);
 
         JPanel buttonsPanel = new JPanel();
-        buttonsPanel.setLayout(new GridLayout(1, 2, 5, 5));
         formPanel.setOpaque(false);
-        buttonsPanel.add(btnClear);
+        buttonsPanel.add(btnPrevious);
+        buttonsPanel.add(btnNext);
+        buttonsPanel.add(btnUpdate);
+        buttonsPanel.add(btnExit);
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
-        mainPanel.setBackground(new Color(128, 128, 255));
-        mainPanel.add(formPanel, BorderLayout.NORTH);
+        mainPanel.add(lbWelcome, BorderLayout.PAGE_START);
+        mainPanel.add(formPanel);
         mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
 
         add(mainPanel);
-
-        setTitle("Welcome");
-        setSize(500, 600);
-        setMinimumSize(new Dimension(300, 400));
+        setTitle("Account Management");
+        setSize(400, 450);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
     }
